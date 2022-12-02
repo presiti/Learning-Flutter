c441f4eaa990743198f53e48a647d3e63eca1d8c
presiti <zinna0347@gmail.com>
2022-12-02 23:39:01 +0900

    project 생성, HomeScreen 파일 생성

---
+++ quiz_app/lib/screen/screen_home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenSatate createSate() => _HomeScreenSatate();
+}
+
+class _HomeScreenSatate extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {}
+}
