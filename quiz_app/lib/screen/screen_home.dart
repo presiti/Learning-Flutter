ea4db554781b46025d4d09d65f1fe431d7f30f6b
presiti <zinna0347@gmail.com>
2022-12-04 22:18:25 +0900

    Home Screen SafeBar 영역 선언 및 기본 디자인 추가

--- quiz_app/lib/screen/screen_home.dart
+++ quiz_app/lib/screen/screen_home.dart
@@ -2,10 +2,24 @@ import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
-  _HomeScreenSatate createSate() => _HomeScreenSatate();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenSatate extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size; // 넓이와 높이를 알아냄
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+        // 기기상단의 노티 바,하단 영역을 침범하지 않는 안전한 영역을 잡아줌.
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text('My quiz app'),
+        backgroundColor: Colors.deepPurple,
+        leading: Container(),
+      ),
+    ));
+  }
 }
